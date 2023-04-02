700708f1dae2732573f027927ce8779faeeccc62
Ken Atopos <z1158229761@outlook.com>
2023-04-02 15:33:43 +0100

    finish modal

--- modal_window/script.js
+++ modal_window/script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModals = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModals.length; i++) {
+  btnOpenModals[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) closeModal();
+  }
+});
