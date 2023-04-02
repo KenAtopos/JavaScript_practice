ec1c12031d4caf5af40f6de85aa9e17b53dc64f1
Ken Atopos <z1158229761@outlook.com>
2023-04-02 14:44:31 +0100

    finish guess_number game

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -1,1 +1,44 @@
-'use strict';
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🚫 No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎊 Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (guess !== number) {
+    if (score >= 0) {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "🔺 Too high!" : "🔻 Too low!";
+    } else {
+      document.querySelector(".message").textContent = "💀 You lost!";
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").style.width = "15rem";
+});
